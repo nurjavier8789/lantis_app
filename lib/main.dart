0900b3185b9b96e94c55920e2db632d117e006a1
nurjavier8789 <nurjavierprasetyo8789@gmail.com>
2024-05-03 09:03:45 +0700

    Add Add-ons
    - Changing app icon
    - Connecting to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
 
 import 'pages/register.dart';
 import 'pages/login.dart';
 
 void main() {
-  // SystemChrome.setSystemUIOverlayStyle(
-  //   const SystemUiOverlayStyle(
-  //     statusBarIconBrightness: Brightness.dark,
-  //     systemNavigationBarColor: Colors.white,
-  //   ),
-  // );
-
   runApp(
     MaterialApp(
+      title: "Lantis Ticketing App",
       initialRoute: '/',
       routes: {
         '/': (context) => const login(),
